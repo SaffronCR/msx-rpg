2433bf124586930a43a211764acfdbf5f0555c3c
SaffronCR <ladypaketilla@gmail.com>
2019-12-06 21:41:29 +0100

    Fixed initial comment.

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 //------------------------------------------------------------------
-// Dungeon Crawler for MSX2 using Fusion-C
+// Dungeon Crawler for MSX2 using Fusion-C.
 //------------------------------------------------------------------
 
 #include <stdio.h>
